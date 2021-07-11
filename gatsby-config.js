c124deb0663d92267cb9ef9b6123ed0da8d58503
Cristian Cristea <cristiancristea.1987@gmail.com>
2021-07-11 10:35:32 +0000

    Set temp domain

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: 'https://www.yourdomain.tld',
+    siteUrl: 'confident-mestorf-a96039.netlify.app',
     title: 'cristiancristea.dev',
     author: 'Cristian Cristea',
   },
